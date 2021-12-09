53768b3788108db6410b14e44644020e266f35b2
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-09 19:11:22 +0530

    Update QRContainer.js

--- src/Component/QRContainer.js
+++ src/Component/QRContainer.js
@@ -1,98 +1,78 @@
-// import React, { Component } from "react";
-// // import QrReader from "react-qr-scanner";
-// // import QrReader from "modern-react-qr-reader";
-// import QRScan from "qrscan";
+import React, { Component } from "react";
+// import QrReader from "react-qr-scanner";
+// import QrReader from "modern-react-qr-reader";
+import QRScan from "qrscan";
 
-// class QRContainer extends React.Component {
-// //   constructor(props) {
-// //     super(props);
-// //     this.state = {
-// //       delay: 100,
-// //       result: "No result",
-// //     };
-
-// //     this.handleScan = this.handleScan.bind(this);
-// //   }
-// //   handleScan(data) {
-// //     console.log("data :", data);
-// //     if (data) {
-// //       this.setState({
-// //         result: data.text,
-// //       });
-// //     }
-// //   }
-// //   handleError(err) {
-// //     console.error(err);
-// //   }
-// //   render() {
-// //     const previewStyle = {
-// //       height: 240,
-// //       width: 320,
-// //     };
-
-// //     return (
-// //       <div>
-// //         <QrReader
-// //           delay={this.state.delay}
-// //           style={previewStyle}
-// //           onError={this.handleError}
-// //           onScan={this.handleScan}
-// //           facingMode={"environment"}
-// //         />
-// //         <p>{this.state.result}</p>
-// //       </div>
-// //     );
-// //   }
-
+class QRContainer extends React.Component {
 //   constructor(props) {
 //     super(props);
-//     this.state = { value: "", watching: false };
-//     this.onFind = this.onFind.bind(this);
-//   }
+//     this.state = {
+//       delay: 100,
+//       result: "No result",
+//     };
 
-//   onFind(value) {
-//     this.setState({ value, watching: false });
+//     this.handleScan = this.handleScan.bind(this);
 //   }
-
+//   handleScan(data) {
+//     console.log("data :", data);
+//     if (data) {
+//       this.setState({
+//         result: data.text,
+//       });
+//     }
+//   }
+//   handleError(err) {
+//     console.error(err);
+//   }
 //   render() {
+//     const previewStyle = {
+//       height: 240,
+//       width: 320,
+//     };
+
 //     return (
-//       <>
-//         <h1>QRScan Demo</h1>
-//         {this.state.watching ? (
-//           <QRScan onFind={this.onFind} />
-//         ) : (
-//           <>
-//             <button onClick={() => this.setState({ watching: true })}>
-//               Scan
-//             </button>
-//             <h4>value: {this.state.value}</h4>
-//           </>
-//         )}
-//       </>
+//       <div>
+//         <QrReader
+//           delay={this.state.delay}
+//           style={previewStyle}
+//           onError={this.handleError}
+//           onScan={this.handleScan}
+//           facingMode={"environment"}
+//         />
+//         <p>{this.state.result}</p>
+//       </div>
 //     );
 //   }
-// }
 
-// export default QRContainer;
+  constructor(props) {
+    super(props);
+    this.state = { value: "", watching: false };
+    this.onFind = this.onFind.bind(this);
+  }
 
-import React from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
+  onFind(value) {
+    this.setState({ value, watching: false });
+  }
 
-export default function QRContainer() {
-  const [data, setData] = React.useState("Not Found");
-
-  return (
-    <>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <h1>QRScan Demo</h1>
+        {this.state.watching ? (
+          <QRScan onFind={this.onFind} />
+        ) : (
+          <>
+            <button onClick={() => this.setState({ watching: true })}>
+              Scan
+            </button>
+            <h4>value: {this.state.value}</h4>
+          </>
+        )}
+      </>
+    );
+  }
 }
+
+export default QRContainer;
+
 
