f7375ee4884c2854d813e08c491168d3be26bf94
Sahil-020 <80909385+Sahil-020@users.noreply.github.com>
2021-12-09 18:57:42 +0530

    Update QRContainer.js

--- src/Component/QRContainer.js
+++ src/Component/QRContainer.js
@@ -1,76 +1,98 @@
-import React, { Component } from "react";
-// import QrReader from "react-qr-scanner";
-// import QrReader from "modern-react-qr-reader";
-import QRScan from "qrscan";
+// import React, { Component } from "react";
+// // import QrReader from "react-qr-scanner";
+// // import QrReader from "modern-react-qr-reader";
+// import QRScan from "qrscan";
 
-class QRContainer extends React.Component {
+// class QRContainer extends React.Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.state = {
+// //       delay: 100,
+// //       result: "No result",
+// //     };
+
+// //     this.handleScan = this.handleScan.bind(this);
+// //   }
+// //   handleScan(data) {
+// //     console.log("data :", data);
+// //     if (data) {
+// //       this.setState({
+// //         result: data.text,
+// //       });
+// //     }
+// //   }
+// //   handleError(err) {
+// //     console.error(err);
+// //   }
+// //   render() {
+// //     const previewStyle = {
+// //       height: 240,
+// //       width: 320,
+// //     };
+
+// //     return (
+// //       <div>
+// //         <QrReader
+// //           delay={this.state.delay}
+// //           style={previewStyle}
+// //           onError={this.handleError}
+// //           onScan={this.handleScan}
+// //           facingMode={"environment"}
+// //         />
+// //         <p>{this.state.result}</p>
+// //       </div>
+// //     );
+// //   }
+
 //   constructor(props) {
 //     super(props);
-//     this.state = {
-//       delay: 100,
-//       result: "No result",
-//     };
+//     this.state = { value: "", watching: false };
+//     this.onFind = this.onFind.bind(this);
+//   }
 
-//     this.handleScan = this.handleScan.bind(this);
+//   onFind(value) {
+//     this.setState({ value, watching: false });
 //   }
-//   handleScan(data) {
-//     console.log("data :", data);
-//     if (data) {
-//       this.setState({
-//         result: data.text,
-//       });
-//     }
-//   }
-//   handleError(err) {
-//     console.error(err);
-//   }
+
 //   render() {
-//     const previewStyle = {
-//       height: 240,
-//       width: 320,
-//     };
-
 //     return (
-//       <div>
-//         <QrReader
-//           delay={this.state.delay}
-//           style={previewStyle}
-//           onError={this.handleError}
-//           onScan={this.handleScan}
-//           facingMode={"environment"}
-//         />
-//         <p>{this.state.result}</p>
-//       </div>
+//       <>
+//         <h1>QRScan Demo</h1>
+//         {this.state.watching ? (
+//           <QRScan onFind={this.onFind} />
+//         ) : (
+//           <>
+//             <button onClick={() => this.setState({ watching: true })}>
+//               Scan
+//             </button>
+//             <h4>value: {this.state.value}</h4>
+//           </>
+//         )}
+//       </>
 //     );
 //   }
+// }
 
-  constructor(props) {
-    super(props);
-    this.state = { value: "", watching: false };
-    this.onFind = this.onFind.bind(this);
-  }
+// export default QRContainer;
 
-  onFind(value) {
-    this.setState({ value, watching: false });
-  }
+import React from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
-  render() {
-    return (
-      <>
-        <h1>QRScan Demo</h1>
-        {this.state.watching ? (
-          <QRScan onFind={this.onFind} />
-        ) : (
-          <>
-            <button onClick={() => this.setState({ watching: true })}>
-              Scan
-            </button>
-            <h4>value: {this.state.value}</h4>
-          </>
-        )}
-      </>
-    );
-  }
+export default function QRContainer() {
+  const [data, setData] = React.useState("Not Found");
+
+  return (
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
+    </>
+  );
 }
 
-export default QRContainer;
